091f7a942c1d4968e935c57430eb4da444d9fa91
Permanaff <fajar.permanaf314@gmail.com>
2024-07-12 13:10:34 +0700

    Menambahkan Login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [AuthController::class, 'index']);
+Route::post('login', [AuthController::class, 'login']);
+
+
+Route::get('/register', function () {
+    return view('daftar');
+});
+
+Route::get('/helo', function () {
+    return view('daftar');
 });
